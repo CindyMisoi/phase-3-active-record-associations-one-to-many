c6ee8e3bd145365064fd82c885fbd28517d3c075
CindyMisoi <crmisoi2k@gmail.com>
2023-02-25 20:47:48 +0300

    done

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base
-  
+#   macro
+# name after the symbol must be plural
+has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
-  
+    # def game
+    #     # self is the instance of review
+    #     Game.find(self.game_id)
+    # end
+    # use maco to make it easier
+    # the name after the symbol must be singular
+    belongs_to :game
 end
